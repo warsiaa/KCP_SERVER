f9a90a43e76bcb5323d5db87f8a7aea236ba3467
Warsia <kivancmelisenes@gmail.com>
2025-12-26 16:31:27 +0300

    Update MessageType.cs

--- KCP_SERVER/Protocol/MessageType.cs
+++ KCP_SERVER/Protocol/MessageType.cs
@@ -1,10 +1,12 @@
-﻿namespace KCP_SERVER.Protocol
+namespace KCP_SERVER.Protocol
 {
     public enum MessageType : byte
     {
         Hello = 1,
         HelloAck = 2,
         Ping = 3,
-        Pong = 4
+        Pong = 4,
+        VersionCheckRequest = 5,
+        VersionCheckResponse = 6
     }
 }
